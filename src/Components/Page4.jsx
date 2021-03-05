7ba3002a79925a2ad4a385d22651557761aba739
Suraj Prabhu <prabhusuraj103@gmail.com>
2021-03-06 00:10:39 +0530

    made last pages responsive.
    Some changes are still remaining.

--- src/Components/Page4.jsx
+++ src/Components/Page4.jsx
@@ -11,129 +11,97 @@ const Page4 = () => {
     <>
       <section id="about-us">
         <div className="container">
-        <h1 id="title">About Us</h1>
-        <div id="para-container">
-        <p id="first-para">
-          We have set a goal to develop a real time system to prevent users from
-          most common and serious threat of malicious URLs to cybersecurity by
-          detecting such URLs using machine learning.
-        </p>
-        <p id="second-para">
-          Malicious URL or website is a common and serious threat to
-          cybersecurity. Malicious URLs host unsolicited content like spam,
-          phishing, drive-by exploits, etc. and lure unsuspecting users to
-          become victims of scams like monetary loss, theft of private
-          information, and malware installation, and cause losses of billions of
-          dollars every year. It is imperative to detect and act on such threats
-          in a timely manner. Traditionally, this detection is done mostly
-          through the usage of blacklists. However,blacklists cannot be
-          exhaustive, and lack the ability to detect newly generated malicious
-          URLs. To improve the generality of malicious URL detectors, machine
-          learning techniques have been explored with increasing attention in
-          recent years.
-        </p>
-        <p id="third-para">
-          And hence we have created a real time formal solution which is
-          available as website on which you are reading about us, an android
-          application and also a chrome extension. All these based on machine
-          learning approach which assure 90% accuracy on scrutinizing of URLs.
-        </p>
-        <p id="last-para">#besafewithus</p>
-        </div>
-        
-        <p id="team-title">meet our team</p>
-        <div id="member-container">
-          <div id="member-1">
-            <div id="circle-1"><img src={logo} id="mem-logo"/></div>
-            <p id="name">Adarsh Pednekar</p>
-            <div id="logos">
-              <a href="https://github.com/adarshped1525"><GitHubIcon
-                style={{
-                  color: "white",
-                  fontSize: "40px",
-                  marginRight: "20px",
-                }}
-              />
-              </a>
-              <a href="https://www.linkedin.com/in/adarsh-pednekar-11ba55183/"><LinkedInIcon
-                style={{
-                  color: "white",
-                  fontSize: "40px",
-                  marginRight: "20px",
-                }}
-              /></a>
-              <InstagramIcon
-                style={{
-                  color: "white",
-                  fontSize: "40px",
-                  marginRight: "20px",
-                }}
-              />
+          <h1 className="title">About Us</h1>
+          <div className="para-container">
+            <p className="para">
+              We have set a goal to develop a real time system to prevent users
+              from most common and serious threat of malicious URLs to
+              cybersecurity by detecting such URLs using machine learning.
+            </p>
+            <p className="para">
+              Malicious URL or website is a common and serious threat to
+              cybersecurity. Malicious URLs host unsolicited content like spam,
+              phishing, drive-by exploits, etc. and lure unsuspecting users to
+              become victims of scams like monetary loss, theft of private
+              information, and malware installation, and cause losses of
+              billions of dollars every year. It is imperative to detect and act
+              on such threats in a timely manner. Traditionally, this detection
+              is done mostly through the usage of blacklists. However,blacklists
+              cannot be exhaustive, and lack the ability to detect newly
+              generated malicious URLs. To improve the generality of malicious
+              URL detectors, machine learning techniques have been explored with
+              increasing attention in recent years.
+            </p>
+            <p className="para">
+              And hence we have created a real time formal solution which is
+              available as website on which you are reading about us, an android
+              application and also a chrome extension. All these based on
+              machine learning approach which assure 90% accuracy on
+              scrutinizing of URLs.
+            </p>
+            <p className="para">#besafewithus</p>
+          </div>
+
+          <p className="title">meet our team</p>
+          <div className="member-container">
+            <div className="member">
+              <div className="circle">
+                <img src={logo} className="mem-logo" />
+              </div>
+              <p className="name">Adarsh Pednekar</p>
+              <div className="logos">
+                <a href="https://github.com/adarshped1525">
+                  <GitHubIcon className="logo" />
+                </a>
+                <a href="https://www.linkedin.com/in/adarsh-pednekar-11ba55183/">
+                  <LinkedInIcon className="logo" />
+                </a>
+                <InstagramIcon className="logo" />
+              </div>
+            </div>
+            <div className="member">
+              <div className="circle">
+                <img src={logo} className="mem-logo" />
+              </div>
+              <p className="name">Sushant Pagam</p>
+              <div className="logos">
+                <a href="https://github.com/SushantP199">
+                  <GitHubIcon className="logo" />
+                </a>
+                <a href="https://www.linkedin.com/in/sushant-pagam/">
+                  <LinkedInIcon  className="logo"/>
+                </a>
+                <a>
+                  <InstagramIcon className="logo" />
+                </a>
+              </div>
+            </div>
+            <div className="member">
+              <div className="circle">
+                <img src={logo} className="mem-logo" />
+              </div>
+              <p className="name">Suraj Prabhu</p>
+              <div className="logos">
+                <a href="https://github.com/sungod12">
+                  <GitHubIcon className="logo" />
+                </a>
+                <a href="https://www.linkedin.com/in/suraj-prabhu-228475188/">
+                  <LinkedInIcon className="logo"/>
+                </a>
+                <a>
+                  <InstagramIcon className="logo" />
+                </a>
+              </div>
             </div>
           </div>
-          <div id="member-2">
-            <div id="circle-2"><img src={logo} id="mem-logo"/></div>
-            <p id="name">Sushant Pagam</p>
-            <div id="logos">
-              <a href="https://github.com/SushantP199"><GitHubIcon
-                style={{
-                  color: "white",
-                  fontSize: "40px",
-                  marginRight: "20px",
-                }}
-              />
-              </a>
-              <a href="https://www.linkedin.com/in/sushant-pagam/"><LinkedInIcon
-                style={{
-                  color: "white",
-                  fontSize: "40px",
-                  marginRight: "20px",
-                }}
-              /></a>
-              <a><InstagramIcon
-                style={{
-                  color: "white",
-                  fontSize: "40px",
-                  marginRight: "20px",
-                }}
-              /></a>
-            </div>
+          <hr />
+          <div className="bottom-container">
+            <p className="bottom-text bottom-text-1">
+              teamlinkscrutinizer@gmail.com
+            </p>
+            <p className="bottom-text">privacy policy</p>
+            <p className="bottom-text">copyright {year} @ link scrutinizer </p>
           </div>
-          <div id="member-3">
-            <div id="circle-3"><img src={logo} id="mem-logo"/></div>
-            <p id="name">Suraj Prabhu</p>
-            <div id="logos">
-              <a href="https://github.com/sungod12"><GitHubIcon
-                style={{
-                  color: "white",
-                  fontSize: "40px",
-                  marginRight: "20px",
-                }}
-              />
-              </a>
-              <a href="https://www.linkedin.com/in/suraj-prabhu-228475188/"><LinkedInIcon
-                style={{
-                  color: "white",
-                  fontSize: "40px",
-                  marginRight: "20px",
-                }}
-              /></a>
-             <a><InstagramIcon
-                style={{
-                  color: "white",
-                  fontSize: "40px",
-                  marginRight: "20px",
-                }}
-              /></a>
-            </div>
-          </div>
-        </div>
-        <hr />
-        <div id="bottom-container">
-          <p id="email">teamlinkscrutinizer@gmail.com</p>
-          <p id="privacy">privacy policy</p>
-          <p id="copyright">copyright {year} @ link scrutinizer </p>
-        </div>
         </div>
       </section>
     </>
