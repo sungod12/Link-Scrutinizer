0c25e8ba6f7383f183b8042160b808e327298661
Suraj Prabhu <prabhusuraj103@gmail.com>
2021-03-12 12:34:16 +0530

    implemeted loading animation and other minor changes

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -3,10 +3,14 @@ import React, { useState } from "react";
 import "../css/index.css";
 import True from "./True";
 import False from "./False";
+import Loader from "react-loader-spinner";
 
 const HomePage = () => {
   const [stat, getStat] = useState("");
+  const [hidden,changeProp]=useState(false);
   const fetchResult = async () => {
+    getStat("");
+    changeProp(true);
     let result = document.querySelector("input").value;
     let param = {
       method: "POST",
@@ -21,6 +25,7 @@ const HomePage = () => {
     let response = await fetch(urlToUser, param);
     let res = await response.json();
     getStat(res.scrutiny["status"]);
+    changeProp(false);
   };
   return (
     <>
@@ -33,6 +38,7 @@ const HomePage = () => {
           </button>
         </div>
         <div className="result-container">
+          <Loader type="Circles" color="white" visible={hidden}/>
           {
             stat !== "" ? (
             stat.includes("Safe") ? (
